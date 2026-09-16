5bb6efe5c9cd2f7e9ca28c67db1406328ff49b16
susung5202 <74191151+susung5202@users.noreply.github.com>
2026-09-16 11:40:37 +0900

    Show primary navigation on mobile

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import PushSoundListener from "@/components/PushSoundListener";
+import MobileNav from "@/components/MobileNav";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -24,9 +25,10 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">
+      <body className="min-h-full flex flex-col pb-20 md:pb-0">
         <PushSoundListener />
         {children}
+        <MobileNav />
       </body>
     </html>
   );
